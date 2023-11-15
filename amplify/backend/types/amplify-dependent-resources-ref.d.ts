1f548a0caa26bbc9fd995e65eba4e7c11ac6be82
BuildTools <unconfigured@null.spigotmc.org>
2023-11-14 20:44:36 -0500

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s33241cf00": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
